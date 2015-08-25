86632d4fe7614564b3c002a01296ebb2a6de2d96
Raoul DIFFOUO <diraulo@gmail.com>
2015-08-25 05:33:44 +0200

    add english locales

--- lib/shoppe/payfast/engine.rb
+++ lib/shoppe/payfast/engine.rb
@@ -1,6 +1,10 @@
 module Shoppe
   module Payfast
     class Engine < Rails::Engine
+      config.before_initialize do
+        config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+      end
+
       initializer 'shoppe.payfast.initializer' do
         Shoppe::Payfast.setup
       end
